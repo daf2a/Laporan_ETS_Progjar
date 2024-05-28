7e2a87a2293feb39fe6252098a7ead1648308d29
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-05-28 21:58:49 +0700

    Remove unnecessary code and optimize server performance

--- src/server_process_http.py
+++ src/server_process_http.py
@@ -1,21 +1,19 @@
-import os
 from socket import *
 import socket
+import threading
 import time
 import sys
 import logging
-import ssl
-import multiprocessing
 from http import HttpServer
 
 httpserver = HttpServer()
 
 
-class ProcessTheClient(multiprocessing.Process):
+class ProcessTheClient(threading.Thread):
 	def __init__(self, connection, address):
 		self.connection = connection
 		self.address = address
-		multiprocessing.Process.__init__(self)
+		threading.Thread.__init__(self)
 
 	def run(self):
 		rcv=""
@@ -47,34 +45,23 @@ class ProcessTheClient(multiprocessing.Process):
 
 
 
-class Server(multiprocessing.Process):
-	def __init__(self,hostname='testing.net'):
+class Server(threading.Thread):
+	def __init__(self):
 		self.the_clients = []
-#------------------------------
-		self.hostname = hostname
-		cert_location = os.getcwd() + '/certs/'
-		self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-		self.context.load_cert_chain(certfile=cert_location + 'domain.crt',
-									 keyfile=cert_location + 'domain.key')
-#---------------------------------
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		multiprocessing.Process.__init__(self)
+		threading.Thread.__init__(self)
 
 	def run(self):
-		self.my_socket.bind(('0.0.0.0', 8443))
-		self.my_socket.listen(1)
+		self.my_socket.bind(('0.0.0.0', 8889))
+		self.my_socket.listen(1000)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
-			try:
-				self.secure_connection = self.context.wrap_socket(self.connection, server_side=True)
-				# logging.warning("connection from {}".format(self.client_address))
-				clt = ProcessTheClient(self.secure_connection, self.client_address)
-				clt.start()
-				self.the_clients.append(clt)
-			except ssl.SSLError as essl:
-				print(str(essl))
+			# logging.warning("connection from {}".format(self.client_address))
 
+			clt = ProcessTheClient(self.connection, self.client_address)
+			clt.start()
+			self.the_clients.append(clt)
 
 
 
